442d043bd52085143c0c4c63734b958d71554a34
Albix1 <82461359+Albix1@users.noreply.github.com>
2022-01-22 22:06:46 +0100

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,5 @@
-// kod na klikniencie
-var myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/grzyb.jpg') {
-      myImage.setAttribute ('src','images/grzyb2.jpg');
-    } else {
-      myImage.setAttribute ('src','images/grzyb.jpg');
-    }
+// tryby
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
 }
-// nazwa
