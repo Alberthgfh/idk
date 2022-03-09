4c727be5d3fcb566243589ab653c2a298ea02873
Albix1 <82461359+Albix1@users.noreply.github.com>
2022-03-09 19:50:25 +0100

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,10 @@
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+    document.getElementById("button").style.background = '#54367';
+}
+
+function myFunction() {
+    var element = document.getElementById("myDIV");
+    element.classList.add("mystyle");
 }
